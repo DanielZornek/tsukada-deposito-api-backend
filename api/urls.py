3a2f8118eb10ec9307731936dd1e0ae1a7363350
DanielZornek <daniel_zornek91@outlook.com>
2026-05-17 20:55:59 -0300

    url de login

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegistroUsuarioView, ProdutoListarView, ProdutoCreateView
+from .views import RegistroUsuarioView, ProdutoListarView, ProdutoCreateView, LoginUsuarioView
 
 urlpatterns = [
     path('produtos/', ProdutoListarView.as_view(), name='listar_produtos'),
     path('produtos/novo/', ProdutoCreateView.as_view(), name='cadastrar_produto'),
     path('usuarios/registrar/', RegistroUsuarioView.as_view(), name='registrar_usuario'),
+    path('usuarios/login/', LoginUsuarioView.as_view(), name='login_usuario'),
 ]
