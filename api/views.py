c49349dd74e5e6c8d374be2b974c4c693470b745
DanielZornek <daniel_zornek91@outlook.com>
2026-05-17 20:14:46 -0300

    atualização ordem de envio dos dados

--- api/views.py
+++ api/views.py
@@ -46,7 +46,7 @@ class ProdutoListarView(APIView):
         return Response(lista_produtos)
 
 class ProdutoCreateView(APIView):
-    parser_classes = (JSONParser, MultiPartParser, FormParser)
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
 
     def post(self, request):
         inicializar_firebase_se_necessario()
