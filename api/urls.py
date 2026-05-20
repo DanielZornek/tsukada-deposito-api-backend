f5c1e37aced03cda6f2a9dc70aaeffd50f7de05e
DanielZornek <daniel_zornek91@outlook.com>
2026-05-20 12:45:42 -0300

    fix: bug nos endpoints no final das urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import RegistroUsuarioView, ProdutoListarView, ProdutoCreateView, LoginUsuarioView
 
 urlpatterns = [
+    path('produtos', ProdutoListarView.as_view(), name='listar_produtos_sem_barra'),
     path('produtos/', ProdutoListarView.as_view(), name='listar_produtos'),
-    path('produtos/novo/', ProdutoCreateView.as_view(), name='cadastrar_produto'),
+    
+    path('produtos/novo', ProdutoCreateView.as_view(), name='cadastrar_produto_sem_barra'), # <-- Sem barra
+    path('produtos/novo/', ProdutoCreateView.as_view(), name='cadastrar_produto'),         # <-- Com barra
+    
     path('usuarios/registrar/', RegistroUsuarioView.as_view(), name='registrar_usuario'),
     path('usuarios/login/', LoginUsuarioView.as_view(), name='login_usuario'),
 ]
